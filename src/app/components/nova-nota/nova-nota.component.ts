6b2b2ec2d0ae5054bf866b265ff4c0117aa350a9
Lincon <linconmartins18@gmail.com>
2024-07-13 17:17:36 -0300

    Add lib to parse date

--- src/app/components/nova-nota/nova-nota.component.ts
+++ src/app/components/nova-nota/nova-nota.component.ts
@@ -1,66 +1,84 @@
-import {Component} from '@angular/core';
-import {NotaService} from "./nota.service";
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {MatFormField, MatOption, MatSelect} from "@angular/material/select";
-import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
-import {MatInput, MatInputModule} from "@angular/material/input";
-import {MatButton} from "@angular/material/button";
-import {NotaMicrosul} from "../home/models/nota-microsul-model";
-import {Router} from "@angular/router";
+import { Component } from '@angular/core';
+import { NotaService } from './nota.service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMomentDateModule, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { NotaMicrosul } from '../home/models/nota-microsul-model';
+import { Router } from '@angular/router';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'DD/MM/YYYY',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
-    selector: 'app-nova-nota',
-    standalone: true,
-    imports: [
-        FormsModule,
-        MatSelect,
-        MatOption,
-        ReactiveFormsModule,
-        MatFormField,
-        MatDatepickerInput,
-        MatInput,
-        MatInputModule,
-        MatDatepickerToggle,
-        MatDatepicker,
-        MatButton
-    ],
-    templateUrl: './nova-nota.component.html',
-    styleUrl: './nova-nota.component.scss'
+  selector: 'app-nova-nota',
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  providers: [
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+    { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } }
+  ],
+  templateUrl: './nova-nota.component.html',
+  styleUrls: ['./nova-nota.component.scss']
 })
 export class NovaNotaComponent {
 
-    formCadastro: FormGroup;
+  formCadastro: FormGroup;
 
-    constructor(private _notaService: NotaService,
-                private _formBuilder: FormBuilder,
-                private _router: Router) {
-        this.formCadastro = this._formBuilder.group({
-            dataEmissao: ['', Validators.required],
-            numeroNf: ['', Validators.required],
-            tipoItem: ['', Validators.required],
-            qtde: ['', Validators.required],
-            nfRetorno: ['']
-        });
+  constructor(private _notaService: NotaService,
+              private _formBuilder: FormBuilder,
+              private _router: Router) {
+    this.formCadastro = this._formBuilder.group({
+      dataEmissao: ['', Validators.required],
+      numeroNf: ['', Validators.required],
+      tipoItem: ['', Validators.required],
+      qtde: ['', Validators.required],
+      nfRetorno: ['']
+    });
+  }
+
+  salvar() {
+    const novaNota: NotaMicrosul = {
+      numeronf: this.f['numeroNf'].value,
+      data: this.f['dataEmissao'].value,
+      tipoItem: this.f['tipoItem'].value,
+      quantidade: this.f['qtde'].value,
+      notaDevolucao: this.f['nfRetorno'].value
     }
 
-    salvar() {
-        const novaNota: NotaMicrosul = {
-            numeronf: this.f['numeroNf'].value,
-            data: this.f['dataEmissao'].value,
-            tipoItem: this.f['tipoItem'].value,
-            quantidade: this.f['qtde'].value,
-            notaDevolucao: this.f['nfRetorno'].value
-        }
+    this._notaService.novaNota(novaNota).subscribe({
+      next: (value) => {
+        console.log(value);
+        this._router.navigate(['/home']);
+      }
+    });
+  }
 
-        this._notaService.novaNota(novaNota).subscribe({
-            next: (value) => {
-                console.log(value);
-                this._router.navigate(['/home']);
-            }
-        });
-    }
-
-    get f() {
-        return this.formCadastro.controls;
-    }
+  get f() {
+    return this.formCadastro.controls;
+  }
 }
